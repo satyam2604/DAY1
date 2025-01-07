96bd048cddb2222d3a709de316bd1f8c8724bc84
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-07 15:58:19 +0530

    initial ccommit

---
+++ Day1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Day1{
+	public static void main(String [] args){
+		System.out.println("Welcome to java");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(add);
+	}
+	
+}
