21b9d424fd06ee48d56765561a170d218744a334
satyam_2604 <satyamagrawalv2604@gmail.com>
2025-01-07 16:04:19 +0530

    ar

--- Day1.java
+++ Day1.java
@@ -6,6 +6,20 @@ class Day1{
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
 	}
-	
+	public static int add(int a, int b){
+		return a+b;
+	}
+	public static int sub(int a, int b){
+		return a-b;
+	}
+	public static int mul(int a, int b){
+		return a*b;
+	}
+	public static int div(int a, int b){
+		return a/b;
+	}
 }
